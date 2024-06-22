172a752228a2c6714464ab9d7b7bd39b7ebd680a
thiagocarvalho93 <thiagopaes1993@gmail.com>
2024-06-22 15:18:54 -0300

    Next prev images based on hours

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,29 +7,31 @@ const hours = generateHourlyIntervals();
 const months = generateMonthAbbreviations();
 
 function App() {
-  const [hour, setHour] = createSignal('00');
+  const [hourIndex, setHourIndex] = createSignal(0);
   const [day, setDay] = createSignal(getCurrentDayOfMonth());
   const [month, setMonth] = createSignal(getCurrentMonthAbbreviation());
   const [year, setYear] = createSignal(getCurrentYear());
-  const [variable, setVariable] = createSignal('prec');
+  const [variable, setVariable] = createSignal('temp');
   const [version, setVersion] = createSignal('g1');
 
-  const prevImage = () => { };
+  const prevImage = () => {
+    setHourIndex((index) => (index > 0 ? index - 1 : hours.length - 1));
+  };
 
   const nextImage = () => {
-
+    setHourIndex((index) => (index < hours.length - 1 ? index + 1 : 0));
   };
 
   return (
     <div class={styles.App}>
       <nav>
-        <h1>Previsão Macaé</h1>
+        <h1>{`${hours[hourIndex()]}:00 ${day()}/${month()}/${year()}`}</h1>
       </nav>
       <div id="app">
         <div id="image-container">
           <img
             id="forecast-image"
-            src={getImagePath(variable(), version(), hour(), day(), month(), year())}
+            src={getImagePath(variable(), version(), hours[hourIndex()], day(), month(), year())}
             alt="Forecast Image"
           />
         </div>
